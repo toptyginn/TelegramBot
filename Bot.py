4415f20f2f896fb0cdf59e2491dca1bcc316ee96
Nick_On_Off <anikonov0307@gmail.com>
2023-11-08 16:38:52 +0700

    callback is wrong

---
+++ Bot.py
@@ -0,0 +1,127 @@
+import telebot
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+import time
+
+
+def if_set(data, if_data, value):
+    if data == if_data:
+        return value
+    else:
+        pass
+
+# Инициализация бота:
+# Создание бота
+bot = telebot.TeleBot('6802450385:AAF9ytn2osGDhEQUKIqp4R3bTDjPuSr7HaM')
+grade = ''
+
+# Запрос
+
+# Клавиатура
+keyboard_grade = InlineKeyboardMarkup()
+keyboard_ABC = InlineKeyboardMarkup()
+keyboard_ABCD = InlineKeyboardMarkup()
+keyboard_AB = InlineKeyboardMarkup()
+keyboard_days = InlineKeyboardMarkup()
+keyboard_now = InlineKeyboardMarkup()
+
+# Инициализация клавиатуры
+
+# Номера классов
+keyboard_grade.add(InlineKeyboardButton('7', callback_data='7'))
+keyboard_grade.add(InlineKeyboardButton('8', callback_data='8'))
+keyboard_grade.add(InlineKeyboardButton('9', callback_data='9'))
+keyboard_grade.add(InlineKeyboardButton('10', callback_data='10'))
+keyboard_grade.add(InlineKeyboardButton('11', callback_data='11'))
+
+# 3 буквы
+keyboard_ABC.add(InlineKeyboardButton('А', callback_data='А'))
+keyboard_ABC.add(InlineKeyboardButton('Б', callback_data='Б'))
+keyboard_ABC.add(InlineKeyboardButton('В', callback_data='В'))
+keyboard_ABC.add(InlineKeyboardButton('Вернуться к классам', callback_data='return'))
+
+# 2 буквы
+keyboard_AB.add(InlineKeyboardButton('А', callback_data='А'))
+keyboard_AB.add(InlineKeyboardButton('Б', callback_data='Б'))
+keyboard_ABC.add(InlineKeyboardButton('Вернуться к классам', callback_data='return'))
+
+# 4 буквы
+keyboard_ABCD.add(InlineKeyboardButton('А', callback_data='А'))
+keyboard_ABCD.add(InlineKeyboardButton('Б', callback_data='Б'))
+keyboard_ABCD.add(InlineKeyboardButton('В', callback_data='В'))
+keyboard_ABCD.add(InlineKeyboardButton('Г', callback_data='Г'))
+keyboard_ABC.add(InlineKeyboardButton('Вернуться к классам', callback_data='return'))
+
+#Сегодня, вчера или завтра выбор дня для расписания
+keyboard_now.add(InlineKeyboardButton('Вчера', callback_data='Вчера'))
+keyboard_now.add(InlineKeyboardButton('Сегодня', callback_data='Сегодня'))
+keyboard_now.add(InlineKeyboardButton('Завтра', callback_data='Завтра'))
+keyboard_now.add(InlineKeyboardButton('Другой день', callback_data='other'))
+keyboard_now.add(InlineKeyboardButton('Назад', callback_data='return'))
+
+# Дни недели
+keyboard_days.add(InlineKeyboardButton('Понедельник', callback_data='0'))
+keyboard_days.add(InlineKeyboardButton('Вторник', callback_data='1'))
+keyboard_days.add(InlineKeyboardButton('Среда', callback_data='2'))
+keyboard_days.add(InlineKeyboardButton('Четверг', callback_data='3'))
+keyboard_days.add(InlineKeyboardButton('Пятница', callback_data='4'))
+keyboard_days.add(InlineKeyboardButton('Суббота', callback_data='5'))
+keyboard_days.add(InlineKeyboardButton('Назад', callback_data='return'))
+
+# Функционал бота
+'''
+@bot.callback_query_handler(func=lambda call: True)
+def handle_callback(call):
+    keyboard.add(InlineKeyboardButton('7', callback_data='7'))
+    keyboard.add(InlineKeyboardButton('8', callback_data='8'))
+    keyboard.add(InlineKeyboardButton('9', callback_data='9'))
+    keyboard.add(InlineKeyboardButton('10', callback_data='10'))
+    keyboard.add(InlineKeyboardButton('11', callback_data='11'))
+    bot.send_message(call.id, 'Выберите свой класс', reply_markup=keyboard)
+    bot.answer_callback_query(call.id, text='Не проигнорил')
+'''
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.send_message(message.chat.id, 'Welcome to my bot!')
+
+@bot.message_handler(commands=['help'])
+def handle_help(message):
+    bot.send_message(message.chat.id, 'Here are some helpful commands:')
+    bot.send_message(message.chat.id, '/start - Start the bot')
+    bot.send_message(message.chat.id, '/help - Show this help message')
+@bot.message_handler(commands=['timesheet'])
+def timesheet(message):
+    bot.send_message(message.chat.id, 'Выберите свой класс', reply_markup=keyboard_grade)
+
+@bot.callback_query_handler(func=lambda call: True)
+def answer(call):
+    global day, grade
+    if call.data == '7':
+        bot.send_message(call.message.chat.id, 'Выберите букву', reply_markup=keyboard_ABC)
+        grade = grade + call.data
+    elif call.data == '8':
+        bot.send_message(call.message.chat.id, 'Выберите букву', reply_markup=keyboard_ABCD)
+        grade = grade + call.data
+    elif call.data == '9':
+        bot.send_message(call.message.chat.id, 'Выберите букву', reply_markup=keyboard_ABCD)
+        grade = grade + call.data
+    elif call.data == '10':
+        bot.send_message(call.message.chat.id, 'Выберите букву', reply_markup=keyboard_ABCD)
+        grade = grade + call.data
+    elif call.data == '11':
+        bot.send_message(call.message.chat.id, 'Выберите букву', reply_markup=keyboard_AB)
+        grade = grade + call.data
+    print(grade)
+    if len(grade) >= 2:
+        bot.send_message(call.message.chat.id, 'Выберите день', reply_markup=keyboard_now)
+        day = if_set(call.data, 'Вчера', time.localtime(time.time()).tm_mday - 1)
+        day = if_set(call.data, 'Сегодня', time.localtime(time.time()).tm_mday)
+        day = if_set(call.data, 'Вчера', time.localtime(time.time()).tm_mday + 1)
+        day = if_set(call.data, 'other', call.data)
+        if day == 'other':
+            bot.send_message(call.message.chat.id, 'Выберите день', reply_markup=keyboard_days)
+        if call.data in range(6):
+            day = call.data
+
+    print(call.data)
+
+bot.polling(none_stop=True)
