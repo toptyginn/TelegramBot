c5bf9be8e9b1301df7ef36068ddb05b6a7991847
Mikhail Nikonov <mniknikonov@sberbank.ru>
2023-11-09 10:18:20 +0300

    coments

--- test_xls.py
+++ test_xls.py
@@ -1,12 +1,11 @@
 from openpyxl import load_workbook
 
+# грузим книгу
 wb = load_workbook('test.xlsx', read_only=True)
-
+# выбираем лист
 ws = wb['Лист1']
-matrix = {}
-users = []
-stands = []
-svc = []
+# печааем заголовок
 print(f'{ws.cell(1,1).value}\t{ws.cell(1,2).value}\t{ws.cell(1,3).value}')
+# осальные сроки
 for row in range(2,ws.max_row):
     print(f'{ws.cell(row,1).value}\t{ws.cell(row,2).value}\t{ws.cell(row,3).value}')
